87c38327c6423d9dc0ecef82c866fe5e6a8d9c3d
Dominic Chen <d.c.ddcc@gmail.com>
2019-03-05 17:17:23 -0500

    add download script

---
+++ download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget http://cucis.ece.northwestern.edu/projects/DMS/DATASETS/APR.tar.gz
+wget http://cucis.ece.northwestern.edu/projects/DMS/DATASETS/ScalParC.tar.gz
+wget http://cucis.ece.northwestern.edu/projects/DMS/DATASETS/utility_mine.tar.gz
